e5327e491e04bc9868a75e2c8378d9c92f56dc38
Muoka <Muoka@DESKTOP-FUP0EOO>
2022-08-15 13:22:37 +0300

    Добавьте файлы проекта.

---
+++ lesson 2/Program.cs
@@ -0,0 +1,54 @@
+﻿
+string ask(string i)
+{
+    Console.WriteLine($"Please, enter your {i}");
+    var info = Console.ReadLine();
+    if (isWord(info))
+    {
+        return $"{i}: {info}\n";
+    }
+    else
+    {
+        Console.WriteLine("Please, enter the valid info");
+        return ask(i);
+    }
+
+}
+
+bool isWord(string s)
+{
+    foreach (char c in s)
+    {
+        if (!Char.IsLetter(c))
+            return false;
+    }
+    return true;
+}
+
+string askAge(string i)
+{
+    Console.WriteLine($"Please, enter your {i}");
+    var info = Console.ReadLine();
+    if (isNumber(info))
+    {
+        return $"{i}: {info}\n";
+    }
+    else
+    {
+        Console.WriteLine("Please, enter the valid info");
+        return askAge(i);
+    }
+
+}
+
+bool isNumber(string s)
+{
+    foreach (char c in s)
+    {
+        if (!int.TryParse(s, out var number) || number > 100 || number < 1)
+            return false;
+    }
+    return true;
+}
+
+Console.WriteLine("\n" + ask("name") + ask("surname") + askAge("age") + ask("hobby"));
