00a04bfc56cd77b96031eebc7aa7993b61876ca7
Mivuka <muokakeinato@gmail.com>
2022-08-22 08:20:29 +0300

    added description of validation, questions moved from AskInfo metod

--- lesson 2/Program.cs
+++ lesson 2/Program.cs
@@ -1,15 +1,25 @@
-﻿
+﻿using System.Text;
+
+var description = new Dictionary<string, string>()
+{
+    ["name"] = "This field may contain only letters",
+    ["surname"] = "This field may contain only letters",
+    ["age"] = "This field may contain only number in the range from 1 to 100",
+    ["hobby"] = "This field may contain only letters",
+};
+
+var answer = new StringBuilder();
+
 string AskInfo(string question, Func<string, bool> validator)
 {
-    Console.WriteLine($"Please, enter your {question}");
     var info = Console.ReadLine();
     if (validator(info))
     {
-        return $"{question}: {info}\n";
+        return $"Your {question}: {info}\n";
     }
     else
     {
-        Console.WriteLine("Please, enter the valid info");
+        Console.WriteLine("Please, enter the valid info. " + description[question]);
         return AskInfo(question, validator);
     }
 }
@@ -23,6 +33,14 @@ bool IsNumber(string s)
 {
     return !(!int.TryParse(s, out var number) || number > 100 || number < 1);
 }
+Console.WriteLine("Please, enter your name");
+answer.Append(AskInfo("name", IsWord));
+Console.WriteLine("Please, enter your surname");
+answer.Append(AskInfo("surname", IsWord));
+Console.WriteLine("Please, enter your age");
+answer.Append(AskInfo("age", IsNumber));
+Console.WriteLine("Please, enter your hobby");
+answer.Append(AskInfo("hobby", IsWord));
 
-Console.WriteLine("\n" + AskInfo("name", IsWord) + AskInfo("surname", IsWord)
-    + AskInfo("age", IsNumber) + AskInfo("hobby", IsWord));
+Console.WriteLine("\n" + answer.ToString());
+
